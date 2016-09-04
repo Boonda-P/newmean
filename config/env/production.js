af1407cdd47197f33cdbf260a17794d48bc2cf4b
gary-johnbrevard <gary@johnbrevard.com>
2016-09-03 20:39:01 -0400

    pushing live

--- config/env/production.js
+++ config/env/production.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   port: process.env.PORT || 80,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod  ',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
     options: {
       user: '',
       pass: ''
